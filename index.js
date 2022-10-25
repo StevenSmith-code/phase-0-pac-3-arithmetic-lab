80e1e6872b85b8898fef31928b99d6fa540c6fc7
Steven <stevensmith3211@gmail.com>
2022-10-25 16:33:33 -0500

    commit tree eabbc243ef6694c7744cb0bdc5f510c81f507482

---
+++ index.js
@@ -0,0 +1,20 @@
+const add = (a,b) => {return a + b}
+const subtract = (a,b) => {return a - b}
+const multiply = (a,b) =>{return a*b}
+const divide = (a,b) =>{return a/b}
+const makeInt = n => {return parseInt(n,10)}
+const preserveDecimal = n => {return parseFloat(n)}
+
+
+
+
+
+
+const increment = (n) => {
+    n++;
+    return n
+}
+const decrement = n =>{
+    n--
+    return n
+}
